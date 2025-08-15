a0be5f5759afe48656633f18c941a0293689e256
Suvin <suvin076@gmail.com>
2025-08-15 22:23:05 +0100

    Add Function to convert Quaternions to Euler

--- euler_quat_conversion.py
+++ euler_quat_conversion.py
@@ -20,7 +20,38 @@ def euler_to_quat(roll, pitch, yaw, degrees=False):
     
     return np.array([x, y, z, w])
 
+def quaternion_to_euler(x, y, z, w, degrees=False):
+
+    # Roll
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x*x + y*y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+    
+    # Pitch
+    sinp = np.sqrt(1 + 2 * (w * y - x * z))
+    cosp = np.sqrt(1 - 2 * (w * y - x * z))
+    pitch = 2 * np.arctan2(sinp, cosp) - np.pi / 2
+    
+    # Yaw
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y*y + z*z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+    
+    if degrees:
+        roll = np.rad2deg(roll)
+        pitch = np.rad2deg(pitch)
+        yaw = np.rad2deg(yaw)
+    return roll, pitch, yaw
+
+
 if __name__ == "__main__":
     roll, pitch, yaw = 20, 15, 0 
     q = euler_to_quat(roll, pitch, yaw, degrees=True)
     print("Quaternion:", q)
+
+    x = 0.17216259
+    y = 0.12854321
+    z = -0.02266564
+    w = 0.97638259
+    e = quaternion_to_euler(x, y, z, w, degrees=True)
+    print("Euler angles (degrees):", e)
